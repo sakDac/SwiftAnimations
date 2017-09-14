41a9566a458166379380ad69f32fc07a68168baa
Saket Bhushan <saketbhushan@admins-MacBook-Pro.local>
2017-09-14 11:54:58 +0530

    first commit

--- SwiftAnimations/ViewController.swift
+++ SwiftAnimations/ViewController.swift
@@ -5,21 +5,58 @@
 //  Created by Saket Bhushan on 31/08/17.
 //  Copyright © 2017 Saket Bhushan. All rights reserved.
 //
+// https://github.com/ameizi/awesome-ios-animation
+
+
 
 import UIKit
 
+// didn't find very usefull as it crashes also in some cases.
+import pop
+
+// this one is very usefull.
+// https://github.com/daltoniam/DCAnimationKit
+import DCAnimationKit
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var aniView: UIView!
+    
+    
+    @IBOutlet weak var lblAnimate: UIButton!
+    
+    @IBOutlet weak var aniLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func handleStartBtn(_ sender: UIButton) {
+        self.aniView.tada {
+                self.aniView.hinge {
+                    self.lblAnimate.isHidden = true
+            }
+        }
     }
-
-
+    
+    
+    @IBAction func handleStopBtn(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func handlePanGestureAniView(_ sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translation(in: self.view)
+        
+        sender.view!.center = CGPoint(x: sender.view!.center.x + translation.x, y: sender.view!.center.y + translation.y)
+        
+        sender.setTranslation(CGPoint.zero, in: self.view)
+        
+    }
 }
 
